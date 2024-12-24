a52700b8d3f5f6c94e2fd5c699d027ac93e1d02a
AngelValdez-lpz <angel-valdez63@hotmail.com>
2024-12-23 17:24:57 -0700

    cambie el fromulario por un table para tener una vista mas dinamica

--- Eespresso Patronum/Caliente.cs
+++ Eespresso Patronum/Caliente.cs
@@ -54,7 +54,7 @@ namespace Eespresso_Patronum
                   panelProductos.Controls.Add(btnProducto);
               }
           }
-
+        //probando cambios
 
           private List<Producto> ObtenerProductos()
           {
